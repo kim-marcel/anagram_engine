d5c416335abcea090318aa818ca6b93d4c9b2198
Kim Feichtinger <KimFeichtinger@Kims-MacBook-Pro.local>
2018-03-16 10:50:42 +0000

    refactoring: put related methods in their own file

--- myuser.py
+++ myuser.py
@@ -6,26 +6,3 @@ from anagrams import Anagrams
 class MyUser(ndb.Model):
     # Object of anagrams where all the anagrams of the user are stored
     anagrams = ndb.KeyProperty(kind='Anagrams', repeated=True)
-
-    def addNewAnagram(self, text):
-        generatedKey = Anagrams.generateKey(text)
-        key = self.key.id() + '/' + generatedKey
-        self.anagrams.append(ndb.Key('Anagrams', key))
-        anagram = Anagrams(id=key)
-        anagram.words.append(text)
-        anagram.sortedWord = generatedKey
-        anagram.put()
-        self.put()
-
-    def addToAnagram(self, text, anagramKey):
-        anagram = anagramKey.get()
-        if text in anagram.words:
-            # do nothing: word is already in there
-            logging.debug(text + ' already exists.')
-            pass
-
-        else:
-            # append word
-            anagram.words.append(text)
-            # commit to datastore
-            anagram.put()
